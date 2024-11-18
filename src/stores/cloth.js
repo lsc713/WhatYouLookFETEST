72836c74b61294231e627d0f4e13ba90ffe53e54
lsc713 <lsc7134@naver.com>
2024-11-19 01:51:47 +0900

    implement getClothesRecommendation

--- src/stores/cloth.js
+++ src/stores/cloth.js
@@ -3,5 +3,18 @@ import {ref} from 'vue'
 
 export const useClothStore = defineStore('cloth',()=>{
     const clothes = ref([])
+
+
+    const getClothesRecommendation = async (temperature)=>{
+        try{
+            const response = await axios.get('http://localhost:8080/api/v1/cloth',{
+            params: {temperature}
+            })
+            clothes.value = response.data
+        } 
+        catch (error){
+            console.log('clothes reommendation failed',error)
+        }
+    }
     return {clothes}
 })
